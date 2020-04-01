b658acbf14c338d6eef0466b9c17fa7bcae80cc8
Brian-Fairbanks <59707181+Brian-Fairbanks@users.noreply.github.com>
2020-04-01 10:16:35 -0500

    Landing content (#6)
    * added function/api query to include basic album information when clicking on an artist - currently only from the trending tab
    
    * added related artists.  added transition when selecting new artist

--- logic.js
+++ logic.js
@@ -1,33 +1,32 @@
-function getData(artist){
-    $.ajax({ 
-        url:"https://www.vagalume.com.br/u2/index.js",
-        method:"GET",
-        error:function (xhr, ajaxOptions, thrownError){
-            if(xhr.status==404) {
-                console.log("ERROR: 404");
-            }
-        }
-    })
-    .then(function(response){
-        console.log(response);
-    });
-}
+/*==============================================
+=       Global Variables
+================================================*/
+var landingPage = $("#landing-page");
 
-
+var vagaAuthKey = "9e3c9da5e3a86ef90b7a336db22e59cb";
 // Trending Artists API
-var authKey = "9e3c9da5e3a86ef90b7a336db22e59cb";
-var trendingQuery = 'https://api.vagalume.com.br/rank.php?apikey='+authKey+'&type=art&period=day&scope=internacional&limit=6';
+var trendingQuery = 'https://api.vagalume.com.br/rank.php?apikey='+vagaAuthKey+'&type=art&period=day&scope=internacional&limit=6';
 
 var trendingWell = $("#trendingWell"); 
+var relatedWell = $("#relatedWell"); 
 
+
+//albums
+var albumWell = $("#albumWell");
+var curBandData;
+
+/*==============================================
+=       Functions
+================================================*/
+
+// takes the JSON API for trending artists, and writes the with formatted CSS/HTML to the dom
 function printTrending(response){
     console.log(response);
+    //for each artist...
     for (ranked of response.art.day.internacional){
-        console.log(ranked.name);
-        console.log(ranked.pic_small);
-
+        // print it as a card to the trendingWell 
         trendingWell.append(
-            $("<div/>",{class:"card w-full md:w-1/6 inline-flex md:block items-center text-center bg-green-200"}).append([
+            $("<div/>",{class:"card trending w-full md:w-1/6 inline-flex md:block items-center text-center bg-green-200", 'data-artist':ranked.name}).append([
                 $("<img/>",{src:ranked.pic_small, alt:ranked.name, class:"md:w-full sm:"}),
                 $("<span/>",{text:ranked.name, class:"ml-2"}),
             ])
@@ -35,6 +34,7 @@ function printTrending(response){
     }
 }
 
+// run an ajax call to get the top 6 trending artists from VagaLume API, then pass this to print
 
 function getTrending(){
     $.ajax({ 
@@ -46,4 +46,69 @@ function getTrending(){
     });
 }
 
+
+function printAlbums(albumArray){
+    // console.log(albumArray);
+    albumWell.empty();
+    for(album of albumArray){
+        console.log(album);
+        albumWell.append(
+            $("<div/>",{text:album.desc})
+        );
+    }
+}
+function printRelated(relatedArray){
+    // console.log(albumArray);
+    relatedWell.empty();
+    for(artist of relatedArray){
+        console.log(artist);
+        relatedWell.append(
+            $("<div/>",{text:artist.name})
+        );
+    }
+}
+
+
+
+function getData(artist){
+    $.ajax({ 
+        url:"https://www.vagalume.com.br/"+artist+"/index.js",
+        method:"GET",
+        error:function (xhr, ajaxOptions, thrownError){
+            if(xhr.status==404) {
+                console.log("ERROR: 404");
+            }
+        }
+    })
+    .then(function(response){
+
+
+        //store? this information may actually be useful laterS
+        curBandData = response;
+        console.log(curBandData);
+
+        // print all of the albums & related tracks associated with this artist
+        printAlbums(response.artist.albums.item);
+        printRelated(response.artist.related);
+
+        //hide previous card, and display artist card
+        $("#landing-page").addClass("collapsed",400, function(){
+            $("#artistPage").removeClass("collapsed",400);
+        });
+        
+    });
+}
+
+
+/*==============================================
+=      Global Variables
+================================================*/
+
+//start by immediately  showing the ternding artists
 getTrending();
+
+
+//add event listner to each trending buttony
+$(document).on("click",".trending",function(){
+    getData ($(this).attr("data-artist").toLowerCase().trim().replace(" ","-")); 
+})
